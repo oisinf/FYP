3e2d2a6ce90b1284acd27932d49a127f2653aa5b
Oisin <oisinfeelyspoon@gmail.com>
2018-02-13 15:59:58 +0000

    commit

--- TestLogs/pythonVCONF.py
+++
@@ -1,7 +0,0 @@
-f = open("pythonCalib.txt", "w")
-f.write('export ARTOOLKIT5_VCONF="-module=Image  -loop ') 
-for i in range (1,1070):
-	f.write("-image=/home/oisin/libs/TestLogs/CalibrationFrames/"+str(i)+".jpg ")
-
-f.close()
-	
