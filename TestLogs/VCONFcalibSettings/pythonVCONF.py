f572837758e0e59b57d2a6ca525e47ac166ec22b
Oisin <oisinfeelyspoon@gmail.com>
2018-03-31 19:20:03 +0100

    Updates

--- TestLogs/VCONFcalibSettings/pythonVCONF.py
+++
@@ -1,7 +0,0 @@
-f = open("calib7X92.txt", "w")
-f.write('export ARTOOLKIT5_VCONF="-module=Image  -loop ') 
-for i in range (1,1488):
-	f.write("-image=/home/oisin/libs/TestLogs/CalibrationFrames/7X9calib2/"+str(i)+".jpg ")
-
-f.close()
-	
